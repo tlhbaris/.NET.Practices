cecd822af096b3ca941926d8f358f9697fd8585c
tlhbaris <103104761+tlhbaris@users.noreply.github.com>
2022-04-11 10:44:14 +0300

    4.Gun

--- WebApplication8/Controllers/HomeController.cs
+++ WebApplication8/Controllers/HomeController.cs
@@ -1,25 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication8.Models;
-using WebApplication8.Services.SmsService;
 using System.Diagnostics;
 using WebApplication8.Services.EmailService;
 using WebApplication8.Services.SmsService;
-using WebApplication8.Services.EmailService;
 using Mvc101.Services.SmsService;
 
-namespace Mvc101.Controllers
+namespace MVC101.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ISmsService _smsService;
         private readonly IEmailServices _emailService;
         private readonly IWebHostEnvironment _appEnvironment;
+        private readonly IServiceProvider _serviceProvider;
+        private int id;
 
-        public HomeController(ISmsService smsService, IEmailServices emailService, IWebHostEnvironment appEnvironment)
+        public HomeController(ISmsService smsService,
+            IEmailServices emailService,
+            IWebHostEnvironment appEnvironment,
+            IServiceProvider serviceProvider)
         {
             _smsService = smsService;
             _emailService = emailService;
             _appEnvironment = appEnvironment;
+            _serviceProvider = serviceProvider;
+
         }
 
         public IActionResult Index()
@@ -33,8 +38,18 @@ namespace Mvc101.Controllers
             var wissenSms = (WissenSmsService)_smsService;
             Debug.WriteLine(wissenSms.EndPoint);
 
-            var fileStream = new FileStream(@$"{_appEnvironment.WebRootPath}\files\images.jpg", FileMode.Open);
+            IEmailServices emailServices;
+            if (id % 2 == 0)
+            {
+                emailServices = (IEmailServices)_serviceProvider.GetService<SendGridEmailService>();
+            }
+            else
+            {
+                emailServices = (IEmailServices)_serviceProvider.GetService<OutlookEmailService>();
+            }
 
+            //var fileStream = new FileStream(@$"{_appEnvironment.WebRootPath}\files\images.jpg", FileMode.Open);
+           
             _emailService.SendMailAsync(new MailModel()
             {
                 To = new List<EmailModels>()
@@ -42,15 +57,15 @@ namespace Mvc101.Controllers
                     new EmailModels()
                     {
                         Name = "Wissen",
-                        Adress = "site@wissenakademie.com"
+                        Adress = "emrecanyasar1@outlook.com"
                     }
                 },
                 Subject = "Index Açıldı",
                 Body = "Bu emailin body kısmıdır",
-                Attachs = new List<Stream>()
-                {
-                    fileStream
-                }
+                //Attachs = new List<Stream>()
+                //{
+                //    fileStream
+                //}
             });
 
             return View();
--- WebApplication8/Program.cs
+++ WebApplication8/Program.cs
@@ -1,21 +1,21 @@
 
 using WebApplication8.Services.EmailService;
 using SendGrid.Extensions.DependencyInjection;
-using WebApplication8.Services.EmailService;
 using WebApplication8.Services.SmsService;
-using WebApplication8.Services.EmailService;
-using WebApplication8.Services.SmsService;
+
 using Mvc101.Services.SmsService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<ISmsService, WissenSmsService>();
-//builder.Services.AddSendGrid(options =>
-//{
-//    options.ApiKey = "";
-//});
-builder.Services.AddScoped<IEmailServices, OutlookEmailService>();
+builder.Services.AddSendGrid(options =>
+{
+    options.ApiKey = "";
+});
+//builder.Services.AddScoped<IEmailServices, OutlookEmailService>();
+builder.Services.AddScoped<SendGridEmailService>().AddScoped<IEmailServices, SendGridEmailService>(s => s.GetService<SendGridEmailService>());
+builder.Services.AddScoped<OutlookEmailService>().AddScoped<IEmailServices, OutlookEmailService>(s => s.GetService<OutlookEmailService>());
 
 builder.Services.AddControllersWithViews();
 
